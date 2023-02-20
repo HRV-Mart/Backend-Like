470ba2e9f0785aa659f490f6295096d588cc731f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-20 19:05:40 +0530

    Created empty spring project

---
+++ src/main/kotlin/com/example/backendlike/BackendLikeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backendlike
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendLikeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendLikeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/backendlike/BackendLikeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendlike
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendLikeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
