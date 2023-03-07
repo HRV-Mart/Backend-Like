bb11a820d0e8534cefd9f0d82e8f48dc436882a7
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-07 17:33:45 +0530

    Added empty test function

--- src/test/kotlin/com/example/backendlike/controller/LikeControllerTest.kt
+++ src/test/kotlin/com/example/backendlike/controller/LikeControllerTest.kt
@@ -1,4 +1,38 @@
 package com.example.backendlike.controller
 
+import org.junit.jupiter.api.Test
+
 class LikeControllerTest {
+    @Test
+    fun `should insert like in database when not exist in database`() {
+
+    }
+    @Test
+    fun `should not insert like in database when it exist in database`() {
+
+    }
+    @Test
+    fun `should remove like from database when it does not exist in database`() {
+
+    }
+    @Test
+    fun `should not remove like from database if it does not exist in database`() {
+
+    }
+    @Test
+    fun `should return true if like exist in database`() {
+
+    }
+    @Test
+    fun `should return false if like does not exist in database`() {
+
+    }
+    @Test
+    fun `should return all likes by user if it exist in database`() {
+
+    }
+    @Test
+    fun `should return empoty like list by user if it does notx exist in database`() {
+
+    }
 }
