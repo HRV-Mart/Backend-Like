357ae706d6a124d9a42d49899741265795785b8f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-07 18:17:02 +0530

    minor change in unit tests

--- src/test/kotlin/com/example/backendlike/BackendLikeApplicationTests.kt
+++ src/test/kotlin/com/example/backendlike/BackendLikeApplicationTests.kt
@@ -3,7 +3,6 @@ package com.example.backendlike
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class BackendLikeApplicationTests {
 
     @Test
