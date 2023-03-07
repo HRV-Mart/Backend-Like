0d4676ca82cd093e44cd774af4e901d41c2b52e8
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-07 13:29:02 +0530

    Updated like repository

--- src/main/kotlin/com/example/backendlike/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/backendlike/repository/LikeRepository.kt
@@ -3,6 +3,12 @@ package com.example.backendlike.repository
 import com.example.backendlike.model.Like
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
-interface LikeRepository :ReactiveMongoRepository<Like, String>
+interface LikeRepository :ReactiveMongoRepository<Like, String> {
+    fun findLikeByUserID(userId: String): Flux<Like>
+    fun deleteByUserIDAndProductId(userId: String, productId: String): Mono<Void>
+    fun existsByUserIDAndProductId(userId: String, productId: String): Mono<Boolean>
+}
