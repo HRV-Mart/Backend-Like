1eec61b4c3155a73f3af28955085f13d5323eda6
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-07 18:45:32 +0530

    Added application endpoints (#21)
    * Added Like model
    
    * Added server port and mongo db configs in application.properties file
    
    * Added repository
    
    * Created like service
    
    * Updated like repository
    
    * Added controller
    
    * Added custom page request
    
    * Added page request and handle edge cases
    
    * Tested all endpoints
    
    * Add unit tests (#23)
    
    * Created test file
    
    * Added empty test function
    
    * Fixed detekt build fail
    
    * Added one unit test
    
    * Added add like product unit tests
    
    * Added unit tests for deleting like
    
    * Fixed typo
    
    * Added all unit tests
    
    * Remove redundant code
    
    * minor change in unit tests
    
    * Minor change in test
    
    * Update build workflow

---
+++ src/main/kotlin/com/example/backendlike/controller/LikeController.kt
@@ -0,0 +1,57 @@
+package com.example.backendlike.controller
+
+import com.example.backendlike.fixture.CustomPageRequest
+import com.example.backendlike.model.Like
+import com.example.backendlike.service.LikeService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/like")
+class LikeController (
+    @Autowired
+    private val likeService: LikeService
+)
+{
+    @GetMapping("/{userId}")
+    fun getAllLikesOfUser(
+        @PathVariable userId: String,
+        @RequestParam size: Optional<Int>,
+        @RequestParam page: Optional<Int>
+    ) =
+        likeService.getAllLikesOfUser(
+            userId,
+            CustomPageRequest.getPageRequest(
+                optionalSize = size,
+                optionalPage = page
+            )
+        )
+    @GetMapping("/{userId}/{productId}")
+    fun isProductLikedByUser(
+        @PathVariable productId: String,
+        @PathVariable userId: String
+    ) =
+        likeService.likeExistByUserIdAndProductId(
+            userId = userId,
+            productId = productId
+        )
+    @PostMapping()
+    fun addProductToLike(
+        @RequestBody like: Like,
+        response: ServerHttpResponse
+    ) =
+        likeService.addProductToLike(like, response)
+    @DeleteMapping("/{userId}/{productId}")
+    fun removeProductFromLike(
+        @PathVariable productId: String,
+        @PathVariable userId: String,
+        response: ServerHttpResponse
+    ) =
+        likeService.deleteLike(
+            userId = userId,
+            productId = productId,
+            response = response
+        )
+}
---
+++ src/main/kotlin/com/example/backendlike/model/Like.kt
@@ -0,0 +1,12 @@
+package com.example.backendlike.model
+
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("Like")
+@CompoundIndex(name = "like_idx", def = "{'userId': 1, 'productId': 1}",
+    unique = true)
+data class Like (
+    val userId: String,
+    val productId: String
+)
