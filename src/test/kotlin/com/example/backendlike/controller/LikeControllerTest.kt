20cd2f7eae208fc7c04e63b841e1747cbddd8503
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-07 17:45:02 +0530

    Added one unit test

--- src/test/kotlin/com/example/backendlike/controller/LikeControllerTest.kt
+++ src/test/kotlin/com/example/backendlike/controller/LikeControllerTest.kt
@@ -1,11 +1,30 @@
 package com.example.backendlike.controller
 
+import com.example.backendlike.model.Like
+import com.example.backendlike.repository.LikeRepository
+import com.example.backendlike.service.LikeService
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.mock
+import org.springframework.http.server.reactive.ServerHttpResponse
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
 class LikeControllerTest {
+    private val likeRepository = mock(LikeRepository::class.java)
+    private val response = mock(ServerHttpResponse::class.java)
+    private val likeService = LikeService(likeRepository)
+    private val likeController = LikeController(likeService)
+    private val like = Like(
+        userId = "userID",
+        productId = "productID"
+    )
     @Test
     fun `should insert like in database when not exist in database`() {
-
+        doReturn(Mono.just(like)).`when`(likeRepository).insert(like)
+        StepVerifier.create(likeController.addProductToLike(like, response))
+            .expectNext("Like added successfully")
+            .verifyComplete()
     }
     @Test
     fun `should not insert like in database when it exist in database`() {
