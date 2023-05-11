b75e87c435cbf4c21eeb4770f722c63581d66fe6
Harsh3305 <harshverma3305@gmail.com>
2023-05-11 12:55:20 +0530

    Change import

--- src/main/kotlin/com/example/backendlike/controller/LikeController.kt
+++ src/main/kotlin/com/example/backendlike/controller/LikeController.kt
@@ -1,6 +1,6 @@
 package com.example.backendlike.controller
 
-import com.example.backendlike.fixture.CustomPageRequest
+import com.hrv.mart.custompageable.Pageable
 import com.example.backendlike.model.Like
 import com.example.backendlike.service.LikeService
 import org.springframework.beans.factory.annotation.Autowired
