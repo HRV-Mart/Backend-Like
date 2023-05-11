b6a74e65ae9b53f58e59ec902cf318e44e466620
Harsh3305 <harshverma3305@gmail.com>
2023-05-11 12:40:22 +0530

    Remove imports

--- src/main/kotlin/com/example/backendlike/service/LikeService.kt
+++ src/main/kotlin/com/example/backendlike/service/LikeService.kt
@@ -1,6 +1,5 @@
 package com.example.backendlike.service
 
-import com.example.backendlike.fixture.Pageable
 import com.example.backendlike.model.Like
 import com.example.backendlike.repository.LikeRepository
 import org.springframework.beans.factory.annotation.Autowired
