2a141b0befc139c3dd32323606d1655ebd82ceae
Harsh3305 <harshverma3305@gmail.com>
2023-05-22 17:58:39 +0530

    Changes done in code inorder to make it compatible with product library

--- src/main/kotlin/com/example/backendlike/config/ProductConfiguration.kt
+++ src/main/kotlin/com/example/backendlike/config/ProductConfiguration.kt
@@ -2,6 +2,7 @@ package com.example.backendlike.config
 
 import com.hrv.mart.product.repository.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -10,10 +11,12 @@ import org.springframework.web.reactive.function.client.WebClient
 class ProductConfiguration (
     @Autowired
     private val webClientBuilder: WebClient.Builder,
+    @Value("\${product.url}")
+    private val productUrl: String
 )
 {
     @Bean
     fun getProductRepository() =
-        ProductRepository(webClientBuilder)
+        ProductRepository(webClientBuilder, "")
 
 }
