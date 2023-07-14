5e88693d089101747befddd944cdbbd23fd318ae
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-07-14 11:53:32 +0530

    Create comment-on-pr.js

---
+++ .github/scripts/comment-on-pr.js
@@ -0,0 +1,12 @@
+const prNumber = process.env.GITHUB_EVENT.pull_request.number;
+const comment = '@dependabot squash and merge';
+
+const github = require('@actions/github');
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+octokit.rest.issues.createComment({
+  owner: process.env.GITHUB_REPOSITORY_OWNER,
+  repo: process.env.GITHUB_REPOSITORY,
+  issue_number: prNumber,
+  body: comment
+});
